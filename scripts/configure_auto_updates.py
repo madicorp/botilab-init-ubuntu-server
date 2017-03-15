44a4dd4eab7ce705727063ebafb75f32c8227a30
ekougs <ekougs@gmail.com>
2017-03-15 23:30:57 +0000

    configuration de l'auto updates : insérer ligne si non trouvée ; requirements.txt ; fichier de version

--- scripts/configure_auto_updates.py
+++ scripts/configure_auto_updates.py
@@ -3,10 +3,21 @@ from fabric.operations import run
 from scripts.fab_utils import replace_line_in_file
 
 
+def _file_contains_line(file_path, pattern):
+    return run("cat {} | grep '{}'".format(file_path, pattern), warn_only=True, quiet=True)
+
+
 def _replace_in_10_periodic_config(template, value):
-    replace_line_in_file(template.format('.*'),
-                         template.format('"{}"'.format(value)),
-                         '/etc/apt/apt.conf.d/10periodic')
+    periodic_file_path = '/etc/apt/apt.conf.d/10periodic'
+    template_filled_with_value = template.format('"{}"'.format(value))
+    if _file_contains_line(periodic_file_path, template.format('*')):
+        print('contains')
+        replace_line_in_file(template.format('.*'),
+                             template_filled_with_value,
+                             periodic_file_path)
+    else:
+        print('not contains')
+        run("echo '{}' >> {}".format(template_filled_with_value, periodic_file_path))
 
 
 def configure_auto_updates():
--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from distutils.core import setup
 
+from pip.req import parse_requirements
 from setuptools import find_packages
+
+requirements = [str(ir.req) for ir in parse_requirements('requirements.txt')]
+with open('VERSION') as f:
+    version = f.readlines()[0].strip()
 
 setup(
     name='botilab-init-ubuntu-server',
-    version='0.1.0',
+    version=version,
     packages=find_packages(exclude=('venv', 'tests',)),
     url='',
     license='MIT',
     author='Pascal Ekouaghe',
     author_email='ekougs@gmail.com',
     description='Init botilab ubuntu server',
-    install_requires=[
-        'fabric3>=1.13.1',
-    ]
+    install_requires=requirements
 )
